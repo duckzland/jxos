4382fff9629ead40e3d6852ae69ef737d402b081
jason xie <jason.xie@victheme.com>
2019-07-25 02:17:17 +0800

    apache friendly

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         },
         'gatsby-plugin-react-helmet',
         {
-            resolve: 'gatsby-plugin-manifest',
+            resolve: `gatsby-plugin-manifest`,
             options: {
                 name: 'JXOS Mining Operating System',
                 short_name: 'JXOS',
@@ -25,7 +25,49 @@ module.exports = {
                 theme_color: '#000000',
                 display: 'standalone',
                 orientation: 'portrait',
-                icon: 'src/images/favicon.png'
+                icon: 'src/images/favicon.png',
+                icons: [
+                    {
+                        src: `/favicons/icon-48x48.png`,
+                        sizes: `48x48`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-72x72.png`,
+                        sizes: `72x72`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-96x96.png`,
+                        sizes: `96x96`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-144x144.png`,
+                        sizes: `144x144`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-256x256.png`,
+                        sizes: `256x256`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-384x384.png`,
+                        sizes: `384x384`,
+                        type: `image/png`
+                    },
+                    {
+                        src: `/favicons/icon-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`
+                    }
+                ]
             }
         },
         {
@@ -58,6 +100,16 @@ module.exports = {
                     'gatsby-remark-copy-linked-files',
                     'gatsby-remark-smartypants'
                 ]
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-htaccess',
+            options: {
+                RewriteBase: '/',
+                https: true,
+                www: false,
+                SymLinksIfOwnerMatch: true,
+                host: 'https://jxos.work'
             }
         }
         /**{
